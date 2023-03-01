4528d191d52e382642d23cb7259a5c6baede8efc
matthew-pisano <matthewpisano14@gmail.com>
2023-03-01 14:12:13 -0500

    checkpoint

--- Chat.py
+++ Chat.py
@@ -2,3 +2,4 @@
 # save chats from previous sessions and embeded vectors from models
 # recall these vectors when context is needed from previous session and pick similar ones using vector distance
 # Feed decoded text into model to answer question with context
+# https://community.openai.com/t/a-smarter-chatbot-with-memory-idea/27215
