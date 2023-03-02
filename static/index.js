1ee2f7c734274d3760c81d067f71b0cd9f0e1aeb
matthew-pisano <matthewpisano14@gmail.com>
2023-03-01 21:30:01 -0500

    digitalStyle

--- static/index.js
+++ static/index.js
@@ -1,8 +1,14 @@
+USER_SPEAKER = 0
+MATHESIS_SPEAKER = 1
+TYPING = `<p class="speechBubble mathesisSpeech">...</p>`
+
 function hello(){
     console.log("Hello there");
     let inputText = document.getElementById("inputText").value;
     document.getElementById("inputText").value = "";
-    document.getElementById("output").innerHTML += 'Me: '+inputText+"<br>";
+    addOutput(USER_SPEAKER, inputText)
+    // Add typing
+    document.getElementById("output").innerHTML += TYPING
     fetch('/input', {
         method: 'POST',
         headers: {
@@ -12,6 +18,17 @@ function hello(){
         body: JSON.stringify({ "inputText": inputText })
     }).then(response => response.json()).then(response => {
         console.log(JSON.stringify(response));
-        document.getElementById("output").innerHTML += "Mathesis:"+response["content"]+"<br>";
+        // Remove typing
+        let tmp = document.getElementById("output").innerHTML;
+        document.getElementById("output").innerHTML = tmp.replace(TYPING, "");
+        addOutput(MATHESIS_SPEAKER, response["content"])
     });
+}
+
+function addOutput(speaker, text){
+    let speakerName = speaker === USER_SPEAKER ? "Me: " : "Mathesis: ";
+    let bubbleClass = speaker === USER_SPEAKER ? "userSpeech" : "mathesisSpeech";
+    document.getElementById("output").innerHTML += `<p class="speechBubble ${bubbleClass}">
+        ${speakerName+text}
+    </p>`;
 }
