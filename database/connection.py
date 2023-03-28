a6fe8bf26df00696a8eae199e78bf19faf64dda3
Yanshen <yanshenlin1001@gmail.com>
2023-03-27 23:13:50 -0400

    dbs basic done

--- database/connection.py
+++ database/connection.py
@@ -46,9 +46,53 @@ def insert_user(collection, rcsid, name):
 
 
 
-# TODO: Update function (update the chatlog, update the performance)
-# TODO: upload the performance to DBS (and chatlog)
+# Function to update the chatlog history => conversation history. 
+# Parameter are
+    # Collection => database schema
+    # rcsid => user crendentials; need to use the specific rcsid to update the specific chatlog 
+    # title => title for the conversation
+    # data => chatlog data; need to be in list format ==> example_chatlog_data = ['User: xxxxxxxxx1-231-1', 'Bot: xxxxxxxxx1-231-2', 'User: xxxxxxxxx1-231-3', 'Bot: xxxxxxxxx1-231-4']
+def update_chatlog(collection, rcsid, title, data):
+    # Example dictionary outline for the 'Chatlog' 
+    # print(document['Chatlog']['Homework help on Chapter 1'])
+    query = {'RCSid': rcsid}
+    document = collection.find_one(query)
+   
+    if document:
+        chatlog = document['Chatlog']
+        if title in chatlog:
+            chatlog[title].extend(data)
+        else:
 
+            chatlog[title] = data
+        newvalues = {"$set": {'Chatlog': chatlog}}
+        collection.update_one(query, newvalues)
+    else:
+        raise ValueError("Document not found for RCSid: {}".format(rcsid))
+
+
+# Same idea as update_chatlog
+# outcome => dictionary --> see the example performance outcome example
+def update_performance(collection, rcsid, topic, outcome):
+    query = {'RCSid': rcsid}
+    document = collection.find_one(query)
+    print(document['Performance'])
+
+    if document:
+        performance = document['Performance']
+        if topic in performance:
+            existing_outcome = performance[topic]['Outcome']
+            if existing_outcome != outcome:
+                performance[topic] = {"Topic": topic, "Outcome": outcome}
+        else:
+            performance[topic] = {"Topic": topic, "Outcome": outcome}
+
+        collection.update_one(query, {"$set": {'Performance': performance}})
+    else:
+        raise ValueError("Document not found for RCSid: {}".format(rcsid))
+
+        
+    
 
 
 
@@ -56,6 +100,16 @@ if __name__ == '__main__':
     collection = connection()
     data = query_documents(collection)
     # insert_user(collection, "liy123", "Sam")
-
-    for document in data:
-        print(document, "\n")
+    example_chatlog_data = ['User: xxxxxxxxx1-231-1', 'Bot: xxxxxxxxx1-231-2', 'User: xxxxxxxxx1-231-3', 'Bot: xxxxxxxxx1-231-4']
+    example_performance_outcome = {
+        "Question 1": "Correct",
+        "Question 2": "Incorrect",
+        "Question 3": "Correct",
+        "Question 4": "Incorrect",
+        "Question 5": "Correct",
+        "Question 6": "Incorrect"
+    }
+    update_performance(collection, 'liyu123', 'Unit test', example_performance_outcome)
+    # update_chatlog(collection, 'liyu123', "Pop Quiz", example_chatlog_data)
+    # for document in data:
+    #     print(document, "\n")
